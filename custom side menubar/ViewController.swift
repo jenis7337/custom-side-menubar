b28a702ee33537d4b75fea0487545daf2eec327a
jenis navadiya <jenisnavadiya04@gmail.com>
2023-07-09 12:24:43 +0530

    half

--- custom side menubar/ViewController.swift
+++ custom side menubar/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var v: UIView!
+    @IBOutlet weak var xValue: NSLayoutConstraint!
+    @IBOutlet weak var buttonAction: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        v.alpha = 0
+     
     }
-
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        UIView.animate(withDuration: 0.5, delay: 0, options: .curveLinear){
+            self.v.alpha = 1
+        }
+    }
+    @IBAction func buttonAction1(_ sender: Any) {
+        xValue.constant = 280
+        
+    }
+    
+    @IBAction func backButtonAction(_ sender: Any) {
+        xValue.constant = -280
+    }
 }
 
