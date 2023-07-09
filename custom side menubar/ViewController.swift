307034c8f00fd82d4b0e8ec7a3936d96fe50703d
jenis navadiya <jenisnavadiya04@gmail.com>
2023-07-09 12:01:53 +0530

    Initial Commit

---
+++ custom side menubar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  custom side menubar
+//
+//  Created by Jenish Navadiya on 09/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
